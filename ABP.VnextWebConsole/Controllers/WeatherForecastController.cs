a605e874adae837e84e1404f21354e35793417f2
vivian.guo <vivian.guo@starlight-sms.com>
2022-01-19 13:45:37 +0800

    abp.vnext

--- ABP.VnextWebConsole/Controllers/WeatherForecastController.cs
+++ ABP.VnextWebConsole/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ABP.VnextConsole.Common;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,9 @@ namespace ABP.VnextWebConsole.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        public CommonService commonService { get; set; }
+
+
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
